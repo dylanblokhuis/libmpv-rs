7a9fb00d2ff75a6067a7777305fb24f0ea60ba19
Dylan <dylanblokhuis3@hotmail.com>
2023-02-12 11:22:23 +0100

    mpv build

--- libmpv-sys/build.rs
+++ libmpv-sys/build.rs
@@ -16,8 +16,10 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-use std::env;
-use std::path::PathBuf;
+use std::io::{self, Write};
+use std::path::{Path, PathBuf};
+use std::process::Command;
+use std::{env, fs};
 
 #[cfg(not(feature = "use-bindgen"))]
 fn main() {
@@ -37,12 +39,111 @@ fn main() {
     #[cfg(target_env = "msvc")]
     download_and_compile_lib();
 
+    #[cfg(target_os = "linux")]
+    use_mpv_build();
+
     #[cfg(target_env = "msvc")]
     println!("cargo:rustc-link-lib=static=mpv");
-    #[cfg(not(target_env = "msvc"))]
+
+    #[cfg(target_os = "linux")]
+    println!("cargo:rustc-link-lib=static=mpv");
+
+    #[cfg(target_os = "macos")]
     println!("cargo:rustc-link-lib=mpv");
 }
 
+#[cfg(target_os = "linux")]
+fn use_mpv_build() {
+    let install_dir = env::var("OUT_DIR").unwrap() + "/installed";
+    let lib_install_dir = Path::new(&install_dir).join("lib");
+    fs::create_dir_all(&lib_install_dir).unwrap();
+
+    let archive_path = lib_install_dir.join("mpvbuild.zip");
+    if fs::File::open(archive_path.clone()).is_err() {
+        let mpv_zip = "https://github.com/mpv-player/mpv-build/archive/refs/heads/master.zip";
+        let res = reqwest::blocking::get(mpv_zip).unwrap();
+        let bytes = res.bytes().unwrap();
+
+        // write file
+        let mut file = std::fs::File::create(archive_path.clone()).unwrap();
+        file.write_all(&bytes).unwrap();
+    }
+
+    let extracted_files_path = lib_install_dir.join("files");
+    if fs::File::open(lib_install_dir.join("files/rebuild")).is_err() {
+        let archive_file = fs::File::open(archive_path.clone()).unwrap();
+        let mut archive = zip::ZipArchive::new(archive_file).unwrap();
+
+        // panic!("{}", archive_path.display());
+        for i in 0..archive.len() {
+            let mut file = archive.by_index(i).unwrap();
+            let outpath = match file.enclosed_name() {
+                Some(path) => extracted_files_path.join(path),
+                None => continue,
+            };
+
+            {
+                let comment = file.comment();
+                if !comment.is_empty() {
+                    println!("File {i} comment: {comment}");
+                }
+            }
+
+            if (*file.name()).ends_with('/') {
+                println!("File {} extracted to \"{}\"", i, outpath.display());
+                fs::create_dir_all(&outpath).unwrap();
+            } else {
+                println!(
+                    "File {} extracted to \"{}\" ({} bytes)",
+                    i,
+                    outpath.display(),
+                    file.size()
+                );
+                if let Some(p) = outpath.parent() {
+                    if !p.exists() {
+                        fs::create_dir_all(p).unwrap();
+                    }
+                }
+                let mut outfile = fs::File::create(&outpath).unwrap();
+                io::copy(&mut file, &mut outfile).unwrap();
+            }
+
+            // Get and Set permissions
+            #[cfg(unix)]
+            {
+                use std::os::unix::fs::PermissionsExt;
+
+                if let Some(mode) = file.unix_mode() {
+                    fs::set_permissions(&outpath, fs::Permissions::from_mode(mode)).unwrap();
+                }
+            }
+        }
+    }
+
+    if fs::File::open(extracted_files_path.join("mpv-build-master/mpv/build/mpv")).is_err() {
+        // build mpv binary
+        let output = Command::new("sh")
+            .current_dir(extracted_files_path.join("mpv-build-master"))
+            .arg("./rebuild")
+            .arg("-j4")
+            .output()
+            .expect("failed to execute process");
+
+        let output = String::from_utf8(output.stdout).unwrap();
+        if !output.contains("Linking target mpv") {
+            panic!("{}\nFailed to build mpv using mpv-build", output);
+        }
+    }
+
+    println!(
+        "cargo:rustc-link-search=native={}",
+        extracted_files_path
+            .join("mpv-build-master/mpv/build")
+            .display()
+    );
+}
+
+#[cfg(target_env = "msvc")]
 fn download_and_compile_lib() {
     use std::{fs, io::Write, path::Path, process::Command};
 
